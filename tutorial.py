72966dccafbf3dd62a712db40cca621f4b750b68
Juan Plazas <juanplaz3@gmail.com>
2024-03-20 22:40:41 +1100

    Assets

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init() #Initialize pygame module
+
+pygame.display.set_caption("Heroe Cave") #Set the caption at the top of the window
+
+BG_COLOR = (255, 255, 255) #Background color (white)
+WIDTH, HEIGHT = 900, 700
