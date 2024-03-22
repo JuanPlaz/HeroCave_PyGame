a437a811b1fa66db15b1b0f94f1e66095c613335
Juan Plazas <juanplaz3@gmail.com>
2024-03-22 21:06:59 +1100

    Game Setup

--- tutorial.py
+++ tutorial.py
@@ -10,3 +10,27 @@ pygame.display.set_caption("Heroe Cave") #Set the caption at the top of the wind
 
 BG_COLOR = (255, 255, 255) #Background color (white)
 WIDTH, HEIGHT = 900, 700
+FPS = 60 #Frames per second
+PLAYER_VEL = 5 #Player Velocity
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))   
+
+def main(window): #Event loop function
+    clock = pygame.time.Clock()
+    
+    run = True
+    while run:
+        clock.tick(FPS) #Ensure game runs 60 fps
+
+        for event in pygame.event.get():    #Loop to ensure closing game once is closed
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+    quit()
+    
+
+if __name__ == "__main__":
+    main(window)
+
+
