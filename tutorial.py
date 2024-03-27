c583be9ca7e674a0895add2c0d65d29bfbc574a5
Juan Plazas <juanplaz3@gmail.com>
2024-03-27 21:56:15 +1100

    Loading and Splitting Sprite Sheets

--- tutorial.py
+++ tutorial.py
@@ -15,9 +15,38 @@ PLAYER_VEL = 5 #Player Velocity
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))   
 
+def flip(sprites):      #We need to flip the sprites in all the animations when they are facing left.
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]     #True, False, to split only in x axis, not in y.    
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):     #Directory 1 and 2 to load more images. Measures of the images, and we dont need to load multiple directions "False"
+    path = join("assets", dir1, dir2)   #The path of the images we are gonna be loading
+    images = [f for f in listdir(path) if isfile(join(path, f))]    #It is gonna load any file inside the path directory
+
+    all_sprites = {}    #key will be "animation style" and the value is all of the images in that animation
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha() #Loading the sprite sheet using the path to the image, by getting the transparent background thanks to convert alpha
+
+        sprites = []    
+        for i in range(sprite_sheet.get_width() // width):  #now, we need to get all the individual images from the sprite sheets.
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)  #We need to create a "surface" where the individual animation frame will be. This should be the same size of the images (32). Then we have to "draw" our image into our surface and then, we will export that surface.
+            rect = pygame.Rect(i * width, 0, width, height)  #Location of the original image that we want to grab this new frame from. 
+            surface.blit(sprite_sheet, (0,0), rect) #To draw the frame i want from the sprite sheet in the surface, we need the source, the destination and the area we are gonna use.
+            sprites.append(pygame.transform.scale2x(surface))   #it is gonna be 2x larger than default. So, the sprites will be 64x64.
+
+        if direction:       #To do a multidirectional animation we have to add two keys to our dictionary as follows:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites      #This block of code will load our sprite sheets.
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES =   load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)     #We pass both directories with our character sprites, and True to be multidirectional.
 
     def __init__ (self, x, y, width, height):       #Initial player properties
         self.rect = pygame.Rect(x, y, width , height)
@@ -46,16 +75,17 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):    #This function will be called in every frame (in the "while run loop"), so the character will be uptaded constantly.
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY )   #This is gonna be a "realistic gravity" in the game. After 1 sec (60 frames) we will see the graviity in action.        
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY )   #This is gonna be a "realistic gravity" in the game. After 1 sec (60 frames) we will see the graviity in action.        
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        #pygame.draw.rect(win, self.COLOR, self.rect)       #Line of code to draw a rectangle on the screen.
+        self.sprite = self.SPRITES["idle_" + self.direction][0]   #We have to look into the dictionary with the key and select the first frame (0).
+        win.blit(self.sprite, (self.rect.x, self.rect.y))   #To draw in the position of the screen
 
     
-
 
 
 def get_background(name):   #Generating the background
@@ -78,7 +108,6 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()     #it needs to be updated to avoid old drawings on the screen
 
-
 def handle_move(player):
     keys = pygame.key.get_pressed()     #This will catch the key you are pressing in the keyboard
 
@@ -87,8 +116,6 @@ def handle_move(player):
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
-    
-
 
 def main(window): #Event loop function
     clock = pygame.time.Clock()
@@ -104,7 +131,6 @@ def main(window): #Event loop function
             if event.type == pygame.QUIT:
                 run = False
                 break
-
         
         player.loop(FPS)        #We need to call loop function to keep moving in every frame
         handle_move(player)
@@ -112,7 +138,6 @@ def main(window): #Event loop function
 
     pygame.quit()
     quit()
-    
 
 if __name__ == "__main__":
     main(window)
